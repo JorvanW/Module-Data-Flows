ef7c94b207463c36ecd53067c48a3f0058683705
JorvanW <your.email@example.com>
2026-07-30 17:05:42 +0100

    updated code to exercise-3/exercise.js

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -6,3 +6,21 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
+
+
+let Total = 0;   // Have to declare Total as a re-assignable variable 
+
+console.log(
+  `${"QTY".padEnd(8)}${"ITEM".padEnd(20)}${"TOTAL"}` // padEnd/padStart add spaces to the left and right of an object
+);
+
+for (const { itemName, quantity, unitPricePence } of order) {
+  const lineTotal = (quantity * unitPricePence) / 100;  // quantity times unitPricePence divided by 100 to get the currency amount 
+  Total += lineTotal; // '+=' is the addition assignment
+
+  console.log(
+    `${String(quantity).padEnd(8)}${itemName.padEnd(20)}${lineTotal.toFixed(2).padStart(5)}` // "$" is a template literal that tells JS to insert the value of the item into the string  & "toFixed" fixes the string to (2) decimal points 
+  );
+}
+
+console.log(`\nTotal: ${Total.toFixed(2)}`); // "\n" is a newline escape sequence which tells JS to add something to the next line 
