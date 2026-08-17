930918013f55b5912459a6a0daf9a9907b673238
JorvanW <your.email@example.com>
2026-08-17 15:37:44 +0100

    switched event listener from html to javascript

--- debugging/book-library/script.js
+++ debugging/book-library/script.js
@@ -23,6 +23,9 @@ const title = document.getElementById("title");
 const author = document.getElementById("author");
 const pages = document.getElementById("pages");
 const check = document.getElementById("check");
+const submitButton = document.getElementById("submitButton");
+
+submitButton.addEventListener("click", submit);
 
 //check the right input from forms and if its ok -> add the new book (object in array)
 //via Book function and start render function
